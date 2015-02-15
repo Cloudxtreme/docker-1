83ebbf5d027a912c46746ea9b731be8cd5e08c70
Piotr Zduniak <piotr@zduniak.net>
2015-02-15 14:48:57 +0100

    wrong env name changed

--- runners/mailer-develop.sh
+++ runners/mailer-develop.sh
@@ -3,7 +3,7 @@ DEBIAN_FRONTEND=noninteractive
 docker run \
 	-d \
 	-p 25:25 \
-	-e "RETHINKDB_AUTHKEY=d0llad0llabilly4ll_rethinkdb" \
+	-e "RETHINKDB_KEY=d0llad0llabilly4ll_rethinkdb" \
 	-e "RETHINKDB_DB=dev" \
 	-v /opt/api-keys:/keys \
 	--link rethinkdb:rethinkdb \
