e890aae8bf1665cc8cee3dc9b7502c5d7bc9d05e
Piotr Zduniak <piotr@zduniak.net>
2015-03-01 13:57:52 +0100

    Added mailer-golang and removed port from mailer-develop

--- runners/mailer-golang.sh
+++ runners/mailer-golang.sh
@@ -2,11 +2,11 @@ DEBIAN_FRONTEND=noninteractive
 
 docker run \
 	-d \
+	-p 25:25 \
 	-e "RETHINKDB_KEY=d0llad0llabilly4ll_rethinkdb" \
 	-e "RETHINKDB_DB=dev" \
-	-e "LOOKUPD_ADDR=10.8.0.2:4161" \
-	-e "NSQD_ADDR=10.8.0.2:4150" \
+	-e "NSQD_ADDRESS=10.8.0.2:4150" \
 	-v /opt/api-keys:/keys \
 	--link rethinkdb:rethinkdb \
-	--name mailer-develop \
-	registry.lavaboom.io/lavaboom/mailer-develop
+	--name mailer-golang \
+	registry.lavaboom.io/lavaboom/mailer-golang
