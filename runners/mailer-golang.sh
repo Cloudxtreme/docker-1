de5566a4e759c8e9d7aea721370835878e6f9548
Piotr Zduniak <piotr@zduniak.net>
2015-03-01 22:05:01 +0100

    Added lookupd to env vars of mailer-golang

--- runners/mailer-golang.sh
+++ runners/mailer-golang.sh
@@ -6,6 +6,7 @@ docker run \
 	-e "RETHINKDB_KEY=d0llad0llabilly4ll_rethinkdb" \
 	-e "RETHINKDB_DB=dev" \
 	-e "NSQD_ADDRESS=10.8.0.2:4150" \
+	-e "LOOKUPD_ADDRESS=10.8.0.2:4161" \
 	-v /opt/api-keys:/keys \
 	--link rethinkdb:rethinkdb \
 	--name mailer-golang \
