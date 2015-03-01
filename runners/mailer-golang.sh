7b47ae2ed447facc80352f80d7071d8ace79baaa
Piotr Zduniak <piotr@zduniak.net>
2015-03-01 22:12:38 +0100

    SMTP address

--- runners/mailer-golang.sh
+++ runners/mailer-golang.sh
@@ -7,6 +7,7 @@ docker run \
 	-e "RETHINKDB_DB=dev" \
 	-e "NSQD_ADDRESS=10.8.0.2:4150" \
 	-e "LOOKUPD_ADDRESS=10.8.0.2:4161" \
+	-e "SMTP_ADDRESS=10.8.0.2:2525" \
 	-v /opt/api-keys:/keys \
 	--link rethinkdb:rethinkdb \
 	--name mailer-golang \
