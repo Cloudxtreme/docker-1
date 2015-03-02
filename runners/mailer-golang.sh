c719122305bd7be3e9e0ed20cabc4b1b62883a7a
Piotr Zduniak <piotr@zduniak.net>
2015-03-02 21:35:34 +0100

    oops

--- runners/mailer-golang.sh
+++ runners/mailer-golang.sh
@@ -8,7 +8,7 @@ docker run \
 	-e "NSQD_ADDRESS=10.8.0.2:4150" \
 	-e "LOOKUPD_ADDRESS=10.8.0.2:4161" \
 	-e "SMTP_ADDRESS=10.8.0.2:2525" \
-	-e "DKIM_KEY=/opt/api-keys/mailer.key" \
+	-e "DKIM_KEY=/keys/mailer.key" \
 	-e "DKIM_SELECTOR=mailer" \
 	-v /opt/api-keys:/keys \
 	--link rethinkdb:rethinkdb \
