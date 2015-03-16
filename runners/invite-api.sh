de96faaae1b51b6e222c4f7d8c0ecf9b323b0123
Piotr Zduniak <piotr@zduniak.net>
2015-03-16 11:15:20 +0100

    Removed the link between invite-api and rethinkdb, as it's not needed

--- runners/invite-api.sh
+++ runners/invite-api.sh
@@ -5,6 +5,5 @@ docker run \
 	-p 127.0.0.1:10021:8000 \
 	-e "RETHINKDB_KEY=d0llad0llabilly4ll_rethinkdb" \
 	-e "RETHINKDB_ADDRESS=10.8.0.1:28015" \
-	--link rethinkdb:rethinkdb \
 	--name invite-api \
 	registry.lavaboom.io/lavaboom/invite-api
