47a375e3fd10b9f5d37bb920ff16f91ea263499c
Andrei Simionescu <ansimionescu@gmail.com>
2015-07-28 14:40:36 +0200

    API runner

--- api.sh
+++ api.sh
@@ -1,22 +1,20 @@
-# Lavaboom API
-# Author: Andrei Simionescu <andrei@lavaboom.com>
-# Launches the API on port 5000
-
 DEBIAN_FRONTEND=noninteractive
 
+lanIP=$(/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+lookupdIP=$lanIP
 docker run \
 	-d \
-	-p 127.0.0.1:10000:5000 \
+	-p $lanIP:10000:5000 \
 	-e "RETHINKDB_DB=prod" \
 	-e "API_HOST=api.lavaboom.com" \
 	-e "EMAIL_DOMAIN=lavaboom.com" \
 	-v /opt/api-keys:/keys \
 	--name api-master \
 	lavab/api \
-	-redis_address=172.16.0.1:6379 \
+	-redis_address=$lanIP:6379 \
 	-redis_db=1 \
-	-lookupd_address=172.16.0.1:4161 \
-	-nsqd_address=172.16.0.1:4150 \
-	-rethinkdb_address=172.16.0.1:28015 \
+	-lookupd_address=$lookupdIP:4161 \
+	-nsqd_address=$lanIP:4150 \
+	-rethinkdb_address=$lanIP:28015 \
 	-bloom_filter=/keys/bloom.db \
 	-raven_dsn=https://aaab917804ac489ea883e63312bf96ca:bdccb64b394344df8f2eeac8ad90fc81@sentry.lavaboom.io/4
