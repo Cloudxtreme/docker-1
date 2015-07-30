1410201c6d88f06b140e9abfd63642ce5b97458d
root <root@localhost>
2015-07-30 15:48:57 +0200

    Groove

--- groove-webhook.sh
+++ groove-webhook.sh
@@ -1,15 +1,17 @@
 DEBIAN_FRONTEND=noninteractive
 
+lanIP=$(/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+
 docker run \
 	-d \
 	-e "RETHINKDB_DATABASE=prod" \
-	-e "RETHINKDB_ADDRESS=172.16.0.1:28015" \
+	-e "RETHINKDB_ADDRESS=$lanIP:28015" \
 	-e "PRIVATE_KEY=/keys/hello.key" \
 	-e "GROOVE_ADDRESS=lavab@inbox.groovehq.com" \
-	-e "FORWARDING_SERVER=172.16.0.1:2525" \
+	-e "FORWARDING_SERVER=$lanIP:2525" \
 	-e "DKIM_KEY=/keys/default.private" \
 	-v /opt/api-keys:/keys \
-	-p 172.16.0.1:1000:8000 \
+	-p $lanIP:1000:8000 \
 	--restart always \
 	--name groove-webhook \
-	lavab/groove-webhook
+	quay.io/lavab/groove-webhook
